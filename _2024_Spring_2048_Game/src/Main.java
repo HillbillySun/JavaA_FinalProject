475a96abea08c5292ded3d6ea2ab813885073dd9
BillSun <12311019@mail.sustech.edu.cn>
2024-06-04 20:30:25 +0800

    经典弹窗

--- _2024_Spring_2048_Game/src/Main.java
+++ _2024_Spring_2048_Game/src/Main.java
@@ -5,9 +5,21 @@ import view.LoadFrame;
 import view.InitiaFrame;
 import view.LoadFrame;
 import javax.sound.sampled.*;
+import javax.swing.*;
+
+import static javax.swing.UIManager.setLookAndFeel;
 
 public class Main {
     public static void main(String[] args) {
-        LoadFrame.OpenLoad();
+        SwingUtilities.invokeLater(() -> {
+            try {
+                String lookAndFeel = "com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel";
+                UIManager.setLookAndFeel(lookAndFeel);
+                LoadFrame.OpenLoad();
+            } catch (Exception e) {
+                e.printStackTrace();
+                LoadFrame.OpenLoad();
+            }
+        });
     }
 }
